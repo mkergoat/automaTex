20f9042fc54049d38c5535f593686e1856f5e7db
mathkergoat <49691882+mkergoat@users.noreply.github.com>
2020-10-26 16:23:29 +0100

    general overhaul

--- latextemplate.py
+++ latextemplate.py
@@ -39,9 +39,10 @@ class LaTeXReport:
         Creates the titlepage of the document
         """
         elements = {'title': self.title, 'author': self.author, 
-        titlepage = titlepagetemplate.safesubstitute(elements)
+        titlepage = titlepagetemplate.safe_substitute(elements)
         return titlepage
 
+                    
 class LaTeXSectionning:
     """
     This class represents a level of sectionning in LaTeX
@@ -56,8 +57,9 @@ class LaTeXSectionning:
         return levelstr.replace(' ', '')
     
     def insert_sectionning(self, elements):
-    elements.append(self.write())
+        elements.append(self.write())
 
+                    
 class LaTeXFloat:
     """
     This class representents any kind of float (table, figure)
@@ -75,26 +77,43 @@ class LaTeXFloat:
         elements.append(self.write())
                     
     def prepare_env(self):
+        """
+        Generates a template for the float environnement
+        """
         env = Template(r"""
             \begin{${kind}}
             \centering
                 ${float}
-            \caption{caption}
-            \label{label}
+            \caption{${caption}}
+            \label{${label}}
             \end{${kind}}
             """)
         return env
 
+                    
 class LaTeXFigure(LaTeXFloat):
+    """
+    This class specifically represents a figure (image, tikzfigure...)
+    """
     def __init__(self, kind, caption, label, positionning, figure_filename, scalefactor, cropfactors=['', '']):
+        """
+        Constructor of subclass
+        """
         LaTeXFloat.__init__(self, kind, caption, label, positionning)
         self.scale = scalefactor
         self._cropx = cropfactors[0]
         self._cropy = cropfactors[1]
         self._filename = figure_filename
-        
+   
+                    
 class LaTeXTable(LaTeXFloat):
+    """
+    This class specifically represents a table.
+    """
     def __init__(self, kind, caption, label, positionning, style, file=None, column_alignements='c'):
+        """
+        Constructor of subclass
+        """
         LaTexFloat.__init__(self, kind, caption, label, positionning)
         self.style = style
         self._file = file
@@ -109,11 +128,23 @@ class LaTeXTable(LaTeXFloat):
         """)
         return tab_env
                     
-    def add_line_to_table(self, line, lines, colsep=','):
+    def add_line_to_table(self, line, colsep=','):
         """
         Transforms a line into a str with '&' between columns
         """
-        lines.append('&'.join(line.split(colsep)))
+        self.lines.append('&'.join(line.split(colsep)))
+                    
+    def fit_to_table_style(self):
+        """
+        Formats the list of lines in order to conform to the chosen style
+        """
+        if self.style == 'standard':
+            for line in self.lines:
+                    line += r' \hline'
+        elif self.style == 'booktabs':
+             self.lines = [r'\toprule'] + [self.lines[0]] + [r'\midrule'] + self.lines[1:] + ['r\bottomrule']
+        else:
+             print('Style unavailable in this version of automaTeX')
                     
     def table_from_file(self):
          """
@@ -122,6 +153,9 @@ class LaTeXTable(LaTeXFloat):
          f = open(file, 'r')
          header = f.readline()
          number_of_columns = len(header)
+         self.add_line_to_table(header)
+         for line in f.readlines():
+             self.add_line_to_table(line)
          if len(self.column_alignements) == 1:
             self.column_alignement = number_of_columns * self.column_alignement
           else:
